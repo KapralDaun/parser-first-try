51d96b2d2272f886848d94379b9b035b80e857fa
Sergey <klad002@mail.ru>
2022-09-12 23:12:56 +0300

    Апгрейт парсера

--- main.py
+++ main.py
@@ -2,131 +2,113 @@ import requests
 import json
 import sys
 import codecs
+from config import *
+import os
+import math
 
 ''' Функция получения данных о ноутах с сайта МВИДЕО'''
 
 
 def get_data():
-    cookies = {
-        '_ym_uid': '1634827955312353361',
-        'cfidsgib-w-mvideo': 'odyXDdOub1Auvc0wWltECMBCzGUVgxjrHDQmcL4DZ64KuObai1od0fYQfehpX9+hft4vKrtPKup7Ui52PSUvDzrFWYtH5y4jayEgt1SF20OEpm/TosepmKBvgyF8cYMO2IQ03AfVazibs/yPzFWv9AtkKsxtf+huotN1524=',
-        'HINTS_FIO_COOKIE_NAME': '2',
-        'MVID_AB_SERVICES_DESCRIPTION': 'var4',
-        'MVID_ADDRESS_COMMENT_AB_TEST': '2',
-        'MVID_BLACK_FRIDAY_ENABLED': 'true',
-        'MVID_CALC_BONUS_RUBLES_PROFIT': 'false',
-        'MVID_CART_MULTI_DELETE': 'false',
-        'MVID_CATALOG_STATE': '1',
-        'MVID_CITY_ID': 'CityCZ_975',
-        'MVID_FILTER_CODES': 'true',
-        'MVID_FILTER_TOOLTIP': '1',
-        'MVID_FLOCKTORY_ON': 'true',
-        'MVID_GEOLOCATION_NEEDED': 'true',
-        'MVID_GET_LOCATION_BY_DADATA': 'DaData',
-        'MVID_GIFT_KIT': 'true',
-        'MVID_GUEST_ID': '20873892577',
-        'MVID_IS_NEW_BR_WIDGET': 'true',
-        'MVID_KLADR_ID': '7700000000000',
-        'MVID_LAYOUT_TYPE': '1',
-        'MVID_NEW_ACCESSORY': 'true',
-        'MVID_NEW_DESKTOP_FILTERS': 'true',
-        'MVID_NEW_LK_CHECK_CAPTCHA': 'true',
-        'MVID_NEW_LK_OTP_TIMER': 'true',
-        'MVID_NEW_MBONUS_BLOCK': 'true',
-        'MVID_REGION_ID': '1',
-        'MVID_REGION_SHOP': 'S002',
-        'MVID_SERVICES': '111',
-        'MVID_SERVICES_MINI_BLOCK': 'var2',
-        'MVID_TAXI_DELIVERY_INTERVALS_VIEW': 'old',
-        'MVID_TIMEZONE_OFFSET': '3',
-        'MVID_WEBP_ENABLED': 'true',
-        'NEED_REQUIRE_APPLY_DISCOUNT': 'true',
-        'PICKUP_SEAMLESS_AB_TEST': '2',
-        'PRESELECT_COURIER_DELIVERY_FOR_KBT': 'true',
-        'PROMOLISTING_WITHOUT_STOCK_AB_TEST': '2',
-        'searchType2': '3',
-        'popmechanic_sbjs_migrations': 'popmechanic_1418474375998%3D1%7C%7C%7C1471519752600%3D1%7C%7C%7C1471519752605%3D1',
-        '_ym_d': '1655126913',
-        'utm_term': '%D0%B2%D0%B8%D0%B4%D0%B5%D0%BE%20%D0%BC',
-        'gdeslon.ru.__arc_domain': 'gdeslon.ru',
-        'gdeslon.ru.user_id': '04ec5dc3-c1dd-4491-8aba-b3fe2f81c3f1',
-        'tmr_lvidTS': '1634827955373',
-        'tmr_lvid': '4c6bb3d8528742320e1c9a45d0c652d9',
-        'adrcid': 'An-LV32TvFDiw5IjzP38wsA',
-        'flocktory-uuid': '086f9681-a73a-454a-b1bf-dbca13931f38-1',
-        'afUserId': '249ab5ba-35d3-4556-b024-93fc03f23d31-p',
-        '__ttl__widget__ui': '1655127007755-848e774662ba',
-        'CACHE_INDICATOR': 'false',
-        'COMPARISON_INDICATOR': 'false',
-        'MVID_CREDIT_AVAILABILITY': 'true',
-        'MVID_HANDOVER_SUMMARY': 'true',
-        'MVID_LP_SOLD_VARIANTS': '2',
-        'MVID_MCLICK': 'true',
-        'MVID_MINI_PDP': 'true',
-        'MVID_MOBILE_FILTERS': 'true',
-        'MVID_SMART_BANNER_BOTTOM': 'true',
-        'SENTRY_ERRORS_RATE': '0.1',
-        'SENTRY_TRANSACTIONS_RATE': '0.5',
-        'mindboxDeviceUUID': '45a8ad6b-a277-4dea-83aa-1e4df0b6cf7c',
-        'directCrm-session': '%7B%22deviceGuid%22%3A%2245a8ad6b-a277-4dea-83aa-1e4df0b6cf7c%22%7D',
-        '__hash_': '67e6caf7b3f45f04e1ea5ef84a9cc91b',
-        '__lhash_': '4744fb2b237ba1ef43bddfc968333e00',
-        'MVID_CART_AVAILABILITY': 'true',
-        'MVID_MINDBOX_DYNAMICALLY': 'true',
-        'MVID_PROMO_CATALOG_ON': 'true',
-        'flacktory': 'no',
-        '_sp_ses.d61c': '*',
-        '_gid': 'GA1.2.406364971.1662911336',
-        '_ym_isad': '2',
-        '__SourceTracker': 'google__organic',
-        'admitad_deduplication_cookie': 'google__organic',
-        'SMSError': '',
-        'authError': '',
-        'advcake_track_id': 'c8522059-7e7a-954d-cf26-7f8763d1d7ff',
-        'advcake_session_id': '9f21f606-5c7e-342a-d7cc-5c0f57f2c081',
-        'AF_SYNC': '1662911343297',
-        'tmr_detect': '0%7C1662911347225',
-        '_dc_gtm_UA-1873769-1': '1',
-        '_ga': 'GA1.2.270987267.1655126913',
-        '_dc_gtm_UA-1873769-37': '1',
-        'tmr_reqNum': '1276',
-        '_ga_CFMZTSS5FM': 'GS1.1.1662911335.3.1.1662911574.0.0.0',
-        '_ga_BNX5WPP3YK': 'GS1.1.1662911335.3.1.1662911574.53.0.0',
-        '_sp_id.d61c': 'ae7945d6-d12a-4cf7-bda1-d261e4cae19f.1662911336.1.1662911575..b74d27dd-fe75-4e96-82ab-03d4f4eb7690..27798301-bb32-42c5-838f-27a55274d808.1662911335826.3',
-        'MVID_ENVCLOUD': 'prod1',
-        'JSESSIONID': '1x1BjpGCL46cGJnRcvgrSPkWCXrfmjV19112DmVLdCxL1dqzTpCN!1584468038',
-        'bIPs': '-955324074',
-    }
-
-    headers = {
-        'authority': 'www.mvideo.ru',
-        'accept': 'application/json',
-        'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-        'baggage': 'sentry-transaction=%2F,sentry-public_key=1e9efdeb57cf4127af3f903ec9db1466,sentry-trace_id=04938eb3a0864b448f4569dd2ee105da,sentry-sample_rate=0%2C5',
-        # Requests sorts cookies= alphabetically
-        # 'cookie': '_ym_uid=1634827955312353361; cfidsgib-w-mvideo=odyXDdOub1Auvc0wWltECMBCzGUVgxjrHDQmcL4DZ64KuObai1od0fYQfehpX9+hft4vKrtPKup7Ui52PSUvDzrFWYtH5y4jayEgt1SF20OEpm/TosepmKBvgyF8cYMO2IQ03AfVazibs/yPzFWv9AtkKsxtf+huotN1524=; HINTS_FIO_COOKIE_NAME=2; MVID_AB_SERVICES_DESCRIPTION=var4; MVID_ADDRESS_COMMENT_AB_TEST=2; MVID_BLACK_FRIDAY_ENABLED=true; MVID_CALC_BONUS_RUBLES_PROFIT=false; MVID_CART_MULTI_DELETE=false; MVID_CATALOG_STATE=1; MVID_CITY_ID=CityCZ_975; MVID_FILTER_CODES=true; MVID_FILTER_TOOLTIP=1; MVID_FLOCKTORY_ON=true; MVID_GEOLOCATION_NEEDED=true; MVID_GET_LOCATION_BY_DADATA=DaData; MVID_GIFT_KIT=true; MVID_GUEST_ID=20873892577; MVID_IS_NEW_BR_WIDGET=true; MVID_KLADR_ID=7700000000000; MVID_LAYOUT_TYPE=1; MVID_NEW_ACCESSORY=true; MVID_NEW_DESKTOP_FILTERS=true; MVID_NEW_LK_CHECK_CAPTCHA=true; MVID_NEW_LK_OTP_TIMER=true; MVID_NEW_MBONUS_BLOCK=true; MVID_REGION_ID=1; MVID_REGION_SHOP=S002; MVID_SERVICES=111; MVID_SERVICES_MINI_BLOCK=var2; MVID_TAXI_DELIVERY_INTERVALS_VIEW=old; MVID_TIMEZONE_OFFSET=3; MVID_WEBP_ENABLED=true; NEED_REQUIRE_APPLY_DISCOUNT=true; PICKUP_SEAMLESS_AB_TEST=2; PRESELECT_COURIER_DELIVERY_FOR_KBT=true; PROMOLISTING_WITHOUT_STOCK_AB_TEST=2; searchType2=3; popmechanic_sbjs_migrations=popmechanic_1418474375998%3D1%7C%7C%7C1471519752600%3D1%7C%7C%7C1471519752605%3D1; _ym_d=1655126913; utm_term=%D0%B2%D0%B8%D0%B4%D0%B5%D0%BE%20%D0%BC; gdeslon.ru.__arc_domain=gdeslon.ru; gdeslon.ru.user_id=04ec5dc3-c1dd-4491-8aba-b3fe2f81c3f1; tmr_lvidTS=1634827955373; tmr_lvid=4c6bb3d8528742320e1c9a45d0c652d9; adrcid=An-LV32TvFDiw5IjzP38wsA; flocktory-uuid=086f9681-a73a-454a-b1bf-dbca13931f38-1; afUserId=249ab5ba-35d3-4556-b024-93fc03f23d31-p; __ttl__widget__ui=1655127007755-848e774662ba; CACHE_INDICATOR=false; COMPARISON_INDICATOR=false; MVID_CREDIT_AVAILABILITY=true; MVID_HANDOVER_SUMMARY=true; MVID_LP_SOLD_VARIANTS=2; MVID_MCLICK=true; MVID_MINI_PDP=true; MVID_MOBILE_FILTERS=true; MVID_SMART_BANNER_BOTTOM=true; SENTRY_ERRORS_RATE=0.1; SENTRY_TRANSACTIONS_RATE=0.5; mindboxDeviceUUID=45a8ad6b-a277-4dea-83aa-1e4df0b6cf7c; directCrm-session=%7B%22deviceGuid%22%3A%2245a8ad6b-a277-4dea-83aa-1e4df0b6cf7c%22%7D; __hash_=67e6caf7b3f45f04e1ea5ef84a9cc91b; __lhash_=4744fb2b237ba1ef43bddfc968333e00; MVID_CART_AVAILABILITY=true; MVID_MINDBOX_DYNAMICALLY=true; MVID_PROMO_CATALOG_ON=true; flacktory=no; _sp_ses.d61c=*; _gid=GA1.2.406364971.1662911336; _ym_isad=2; __SourceTracker=google__organic; admitad_deduplication_cookie=google__organic; SMSError=; authError=; advcake_track_id=c8522059-7e7a-954d-cf26-7f8763d1d7ff; advcake_session_id=9f21f606-5c7e-342a-d7cc-5c0f57f2c081; AF_SYNC=1662911343297; tmr_detect=0%7C1662911347225; _dc_gtm_UA-1873769-1=1; _ga=GA1.2.270987267.1655126913; _dc_gtm_UA-1873769-37=1; tmr_reqNum=1276; _ga_CFMZTSS5FM=GS1.1.1662911335.3.1.1662911574.0.0.0; _ga_BNX5WPP3YK=GS1.1.1662911335.3.1.1662911574.53.0.0; _sp_id.d61c=ae7945d6-d12a-4cf7-bda1-d261e4cae19f.1662911336.1.1662911575..b74d27dd-fe75-4e96-82ab-03d4f4eb7690..27798301-bb32-42c5-838f-27a55274d808.1662911335826.3; MVID_ENVCLOUD=prod1; JSESSIONID=1x1BjpGCL46cGJnRcvgrSPkWCXrfmjV19112DmVLdCxL1dqzTpCN!1584468038; bIPs=-955324074',
-        'referer': 'https://www.mvideo.ru/noutbuki-planshety-komputery-8/noutbuki-118?from=under_search',
-        'sec-ch-ua': '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-origin',
-        'sentry-trace': '04938eb3a0864b448f4569dd2ee105da-97ed196faa5dcc09-0',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
-        'x-set-application-id': '0bdf7611-ff29-4eed-9de3-77cf51a75a55',
-    }
-
     params = {
-        'categoryId': '118',
+        'categoryId': '181',
         'offset': '0',
         'limit': '24',
         'doTranslit': 'true',
     }
 
-    response = requests.get('https://www.mvideo.ru/bff/products/listing', params=params, cookies=cookies,
-                            headers=headers).json()
+    if not os.path.exists('data'):  # Если не существует директории дата, создаем её
+        os.mkdir('data')
 
+    s = requests.Session()  # Создаем объект сессии и отправим через нее первый запрос
+    response = s.get('https://www.mvideo.ru/bff/products/listing', params=params, cookies=cookies,
+                     headers=headers).json()
+
+    total_items = response.get('body').get('total')  # Получим количество товаров
+
+    if total_items is None:
+        return '[!] There are no items here [!]'
+
+    pages_count = math.ceil(total_items / 24)
+
+    print(f'[INFO] Total items: {total_items} | Total pages: {pages_count}')
+
+    products_ids = {}  # Словарь ид оборудований
+    products_description = {}  # Словарь описания товаров
+    products_price = {}  # Словарь с ценами на товары
+
+    for i in range(pages_count):
+        offset = f'{i * 24}'
+
+        params = {
+            'categoryId': '181',
+            'offset': offset,  # Передаем в параметры оффсет
+            'limit': '24',
+            'doTranslit': 'true',
+        }
+
+        response = s.get('https://www.mvideo.ru/bff/products/listing', params=params, cookies=cookies,
+                         headers=headers).json()
+
+        products_ids_list = response.get('body').get('products')  # Получаем список ид текущей итерации
+        products_ids[i] = products_ids_list  # Запишем список в словарь с ключом = номеру итерации
+        json_data = {  # Формируем второй запрос для получения описаний товаров
+            'productIds': products_ids_list,
+            'mediaTypes': [
+                'images',
+            ],
+            'category': True,
+            'status': True,
+            'brand': True,
+            'propertyTypes': [
+                'KEY',
+            ],
+            'propertiesConfig': {
+                'propertiesPortionSize': 5,
+            },
+            'multioffer': False,
+        }
+
+        response = s.post('https://www.mvideo.ru/bff/product-details/list', cookies=cookies, headers=headers,
+                                 json=json_data).json()
+        products_description[i] = response  # Получаем описание товаров в виде ответа
+
+        products_ids_str = ','.join(products_ids_list)  # Переводим список ид в строку для передачи в запрос
+
+        params = {
+            'productIds': products_ids_str,
+            'addBonusRubles': 'true',
+            'isPromoApplied': 'true',
+        }
+
+        #  Получим данные о ценах по строке ид
+        response = s.get('https://www.mvideo.ru/bff/products/prices', params=params, cookies=cookies,
+                                headers=headers).json()
+
+        material_prices = response.get('body').get('materialPrices')  # Получаем список словарей с ценами
+
+        for item in material_prices:  # Пробежимся по списку и составим словарь, который запишем в JSON
+            item_id = item.get('price').get('productId')
+            item_base_price = item.get('price').get('basePrice')
+            item_sale_price = item.get('price').get('salePrice')
+            item_bonus = item.get('bonusRubles').get('total')
+
+            products_price[item_id] = {
+                'item_basePrice': item_base_price,
+                'item_salePrice': item_sale_price,
+                'item_bonus': item_bonus,
+            }
+
+        print(f'[INFO] Finished {i + 1} of the {pages_count} pages!')
+        with open('data/1_products_ids.json', 'w', encoding="utf-8") as file:  # Запишем ид в файл JSON
+            json.dump(products_ids, file, indent=4, ensure_ascii=False)
+
+        with open('data/2_products_description.json', 'w', encoding="utf-8") as file:  # Запишем ид в файл JSON
+            json.dump(products_description, file, indent=4, ensure_ascii=False)
+
+        with open('data/3_products_prices.json', 'w', encoding="utf-8") as file:  # Запишем ид в файл JSON
+            json.dump(products_price, file, indent=4, ensure_ascii=False)
+"""
     products_ids = response.get('body').get('products')  # Достанем ид товаров
 
     with open('1_products_ids.json', 'w') as file:  # Запишем ид в файл JSON
@@ -192,33 +174,40 @@ def get_data():
         with open('4_items_price.json', 'w', encoding="utf-8") as file:
             json.dump(items_prices, file, indent=4, ensure_ascii=False)
 
+"""
+
 
 def get_result():  # Функция обобщения результатов
-    with open('2_items.json', encoding="utf-8") as file:
+    with open('data/2_products_description.json', encoding="utf-8") as file:  # Открываем файл с описание товаров
         products_data = json.load(file)
 
-    with open('4_items_price.json', encoding="utf-8") as file:
+    with open('data/3_products_prices.json', encoding="utf-8") as file:  # Открываем файл с ценами на товары
         products_prices = json.load(file)
 
-    products_data = products_data.get('body').get('products')
+    for items in products_data.values():  # Пробегаемся циклом по значениям словаря
+        products = items.get('body').get('products')
 
-    for i in products_data:
-        product_id = i.get('productId')
+    # products_data = products_data.get('body').get('products')
 
-        if product_id in products_prices:
-            prices = products_prices[product_id]
+        for i in products:
+            product_id = i.get('productId')
 
-        i['item_basePrice'] = prices.get('item_basePrice')
-        i['item_salePrice'] = prices.get('item_salePrice')
-        i['item_bonus'] = prices.get('item_bonus')
+            if product_id in products_prices:
+                prices = products_prices[product_id]
 
-    with open('5_result.json', 'w', encoding="utf-8") as file:
+            i['item_basePrice'] = prices.get('item_basePrice')
+            i['item_salePrice'] = prices.get('item_salePrice')
+            i['item_bonus'] = prices.get('item_bonus')
+            i['item_link'] = f'https://www.mvideo.ru/products/{i.get("nameTranslit")}-{product_id}'  # Формируем ссылку на товар
+
+    with open('data/4_result.json', 'w', encoding="utf-8") as file:  # Запишем результаты в файл №4
         json.dump(products_data, file, indent=4, ensure_ascii=False)
 
 
 def main():
     get_data()
     get_result()
+
 
 if __name__ == '__main__':
     main()
